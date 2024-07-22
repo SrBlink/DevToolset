487aedd78920c70d1aaf9e2d0e632712c191c335
SrBlink <=>
2024-07-22 20:41:47 -0300

    feat: Incluido função para decodificar texto UTF-8, incluido Converção de sigla para nome de estado.

--- src/services/decode-service.js
+++ src/services/decode-service.js
@@ -4,7 +4,24 @@ function Decode() {
 
 
     function utf8(data) {
-        return data
+        
+        if(!data) return;
+
+        const decodedText = {
+            statusSuccess: '',
+            value: ''
+        }
+
+
+        try {
+            decodedText.value = decodeURIComponent(escape(data));
+            decodedText.statusSuccess = true;
+        } catch (error) {
+            decodedText.statusSuccess = false;
+            decodedText.value = error.message;
+        }
+        
+        return decodedText;
     }
 
     return { utf8 }
